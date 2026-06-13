efa54a993ecad8ac385a93d59eaad7adf22dea69
dalmeida-fullsatck <prfsl.davi@gmail.com>
2026-06-13 15:42:05 -0400

    Bola funcionando, mas sem try cath e sem inserir nome de apostador

--- bolao.cpp
+++ bolao.cpp
@@ -8,7 +8,6 @@ int calcularPontos(int gols1_real, int gols2_real, int gols1_aposta, int gols2_a
     if(gols1_real == gols1_aposta && gols2_real == gols2_aposta){
         return 4;
     }
-
     bool real_empate = (gols1_real == gols2_real);
     bool aposta_empate = (gols1_aposta == gols2_aposta);
     if(real_empate == aposta_empate){
@@ -23,105 +22,84 @@ int calcularPontos(int gols1_real, int gols2_real, int gols1_aposta, int gols2_a
 
 
 int main(){
-   locale::global(std::locale("pt_BR.utf8"));
-   locale::global(std::locale(""));
-   const int num_jogos = 4;
-   const int max_apostadores = 3;
+    locale::global(std::locale("pt_BR.utf8"));
+    locale::global(std::locale(""));
+    const int num_jogos = 4;
+    const int max_apostadores = 3;
     
-   cout << "BEM VINDO AO JOGO ITEAM_BET" << endl;
-   cout << "JOGOS\n1- BRA X MAR\n2- BRA X ESC\n3- BRA X HAI\n";
-   cout << "\n-------------------------------------------------\n";
+    cout << "BEM VINDO AO JOGO DE APOSTA ITEAM_BET" << endl;
+    cout << "\n-------------------MENU-------------------\n" << endl;
+    cout << "JOGOS\n1- BRA X MAR\n2- BRA X ESC\n3- BRA X HAI\n";
+    cout << "\n-------------------------------------------------\n";
    //jogo 1
-   int equipe1,equipe2;
+    int equipe1,equipe2;
    //jogo 2
-   int equipe3,equipe4;
+    int equipe3,equipe4;
    //jogo 3
-   int equipe5, equipe6;
+    int equipe5, equipe6;
+    int total_ap1= 0, total_ap2, total_ap3;
+    int num_apostadores;
+    do{
+    cout << "Quantos apostadores serão? (máx 3): "; cin >> num_apostadores; 
+    }while (num_apostadores < 1 || num_apostadores >3);
 
-
-   int jogogol1 = 3;
-   int jogogol2 = 0;
-   int acertou, pontos = 0;
-  
-//JOGO 1<
-   cout << "---- JOGO 1 -----\n BRA X MAR\n" << "Digite o palpite do time do BRA: " ;
-       cin >> equipe1;
-
-
-   if(equipe1 == jogogol1){
-       acertou = 1;
-       cout << "acertou " << acertou << "\n" ;
- 
-   cout << "\n---- JOGO 1 -----\n  BRA X MAR\n"<< "Digite o palpite do time do MAR: " ;
-   cin >> equipe2;
-
-
-   if(equipe2 == jogogol2 ){
-       acertou=2;
-   }
-
-
-   else if(equipe1 > equipe2 && jogogol1 > jogogol2 ){
-      pontos = pontos + 2;
-   }
-   else if(equipe1 < equipe2 && jogogol1 < jogogol2 ){
-      pontos = pontos + 2;
-   }
-   else if(equipe1 == equipe2 && jogogol1 == jogogol2 ){
-      pontos = pontos + 1;
-   }
-   else if(equipe1 < equipe2 && jogogol1 > jogogol2 ){
-      pontos = 0;
-   }
-   else if(equipe1 > equipe2 && jogogol1 < jogogol2 ){
-      pontos = 0;
-   }    
-   if(acertou == 2){
-       pontos = pontos + 3;
-   }else{
-       cout << "\nPlacar incorreto!";
-   }
-   cout << "Pontuação do user: " << pontos << "\n";
-}   
+    if(num_apostadores>=1){
+//JOGO 1
+        cout << "\nAPOSTADOR 1\n";
+        cout << "---- JOGO 1 -----\n BRA X MAR\n" << "Digite o palpite do time do BRA: ";cin >> equipe1;
+        cout << "\n---- JOGO 1 -----\n  BRA X MAR\n"<< "Digite o palpite do time do MAR: ";cin >> equipe2;
 //JOGO 2
+        cout << "\nAPOSTADOR 1\n";
+        cout << "---- JOGO 2 -----\n BRA X ESC\n" << "Digite o palpite do time do BRA: ";cin >> equipe3;
+        cout << "\n---- JOGO 2 -----\n  BRA X ESC\n"<< "Digite o palpite do time da ESC: " ;cin >> equipe4;
+//JOGO 3
+        cout << "\nAPOSTADOR 1\n";
+        cout << "---- JOGO 3 -----\n BRA X HAI\n" << "Digite o palpite do time do BRA: ";cin >> equipe5;
+        cout << "\n---- JOGO 3 -----\n  BRA X HAI\n"<< "Digite o palpite do time do HAI: ";cin >> equipe6;
+        total_ap1 = calcularPontos(3, 0, equipe1, equipe2)+
+                    calcularPontos(4, 1, equipe3, equipe4)+
+                    calcularPontos(5, 1, equipe5, equipe6);
+    }
+
+    if(num_apostadores>=2){
+//JOGO 1
+        cout << "\nAPOSTADOR 2\n";
+        cout << "---- JOGO 1 -----\n BRA X MAR\n" << "Digite o palpite do time do BRA: ";cin >> equipe1;
+        cout << "\n---- JOGO 1 -----\n  BRA X MAR\n"<< "Digite o palpite do time do MAR: ";cin >> equipe2;
+//JOGO 2
+        cout << "\nAPOSTADOR 2\n";
+        cout << "---- JOGO 2 -----\n BRA X ESC\n" << "Digite o palpite do time do BRA: ";cin >> equipe3;
+        cout << "\n---- JOGO 2 -----\n  BRA X ESC\n"<< "Digite o palpite do time da ESC: " ;cin >> equipe4;
+//JOGO 3
+        cout << "\nAPOSTADOR 2\n";
+        cout << "---- JOGO 3 -----\n BRA X HAI\n" << "Digite o palpite do time do BRA: ";cin >> equipe5;
+        cout << "\n---- JOGO 3 -----\n  BRA X HAI\n"<< "Digite o palpite do time do HAI: ";cin >> equipe6;
+        total_ap2 = calcularPontos(3, 0, equipe1, equipe2)+
+                    calcularPontos(4, 1, equipe3, equipe4)+
+                    calcularPontos(5, 1, equipe5, equipe6);
+    }
+    if(num_apostadores>=3){
+//JOGO 1
+        cout << "\nAPOSTADOR 3\n";
+        cout << "---- JOGO 1 -----\n BRA X MAR\n" << "Digite o palpite do time do BRA: ";cin >> equipe1;
+        cout << "\n---- JOGO 1 -----\n  BRA X MAR\n"<< "Digite o palpite do time do MAR: ";cin >> equipe2;
+//JOGO 2
+        cout << "\nAPOSTADOR 3\n";
+        cout << "---- JOGO 2 -----\n BRA X ESC\n" << "Digite o palpite do time do BRA: ";cin >> equipe3;
+        cout << "\n---- JOGO 2 -----\n  BRA X ESC\n"<< "Digite o palpite do time da ESC: " ;cin >> equipe4;
+//JOGO 3
+        cout << "\nAPOSTADOR 3\n";
+        cout << "---- JOGO 3 -----\n BRA X HAI\n" << "Digite o palpite do time do BRA: ";cin >> equipe5;
+        cout << "\n---- JOGO 3 -----\n  BRA X HAI\n"<< "Digite o palpite do time do HAI: ";cin >> equipe6;
+        total_ap3 = calcularPontos(3, 0, equipe1, equipe2)+
+                    calcularPontos(4, 1, equipe3, equipe4)+
+                    calcularPontos(5, 1, equipe5, equipe6);
+    }
 
 
-
-
-//     cout << "\n---- JOGO 2 -----\n BRA X 2- ESC\n"<< "Digite o palpite do time do BRA: " ;
-//         cin >> gol3;
-
-
-//         if(gol3 == jogogol1){
-//         acertou = 1;
-//         cout << "acertou " << acertou << "\n" ;
-//     }
-//     cout << "\n---- JOGO 2 -----\n- BRA X 2- ESC\n" << "Digite o palpite do time do ESC: " ;
-//         cin >> gol4;
-  
-//         if(gol4 == jogogol2){
-//         acertou = 2;
-//         cout << "acertou " << acertou << "\n" ;
-//         }
-
-
-//         cout << "acertou " << acertou << "\n" ;
-//     if(acertou == 2){
-      
-//     }else{
-//         cout << "\nPlacar incorreto!";
-//     }
-
-
-
-
-// //JOGO 3
-//     cout << "\n---- JOGO 3 -----\n BRA X HAI\n"<< "Digite o palpite do time do BRA: " ;
-//         cin >> gol5;
-//     cout << "\n---- JOGO 3 -----\n BRA X ESC\n"<< "Digite o palpite do time do HAI: " ;
-//         cin >> gol6;
-
-
-}
+  if(num_apostadores>=1) cout << "Pontuação do apostador 1: -  " << total_ap1 << "\n";
+  if(num_apostadores>=2) cout << "Pontuação do apostador 2: -  " << total_ap2 << "\n";
+  if(num_apostadores>=3) cout << "Pontuação do apostador 3: -  " << total_ap3 << "\n";
+}   
 
 
